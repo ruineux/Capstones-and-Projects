67a9e8a67a185e045328d922ff7f038e79620d66
Marcus Morante <mgpmorante@gmail.com>
2025-01-20 11:16:07 +0800

    Updated and Added notebook

--- Automation/Mouse Simulation/MouseMovement.py
+++ Automation/Mouse Simulation/MouseMovement.py
@@ -2,16 +2,15 @@ from random import uniform
 import pyautogui as py
 import time
 
-while True:
-    
-    time.sleep(3)  # Wait for 5 seconds
-    # Move mouse to (720, 360) with a random duration between 0.6 and 2.7 seconds
-    py.moveTo(720, 360, uniform(0.6, 2.7), py.easeOutQuad)
-    
-    time.sleep(2)  # Wait for 5 seconds
-    # Move mouse to (450, 900) with a random duration between 0.6 and 2.7 seconds
-    py.moveTo(450, 900, uniform(0.6, 2.7), py.easeOutBack)
-    
-    time.sleep(3)  # Wait for 5 seconds
-    # Move mouse to (360, 720) with a random duration between 0.6 and 2.7 seconds
-    py.moveTo(360, 720, uniform(0.6, 2.7), py.easeInOutQuad)
+try:
+    while True:
+        
+        py.moveTo(uniform(0, py.size().width), uniform(0, py.size().height), # Screen Size
+                  uniform(0.6, 2.7), # Movement speed
+                  py.easeOutQuad)
+        
+        # Sleep Time
+        time.sleep(uniform(2, 4))
+
+except KeyboardInterrupt:
+    print("Script terminated by user")
